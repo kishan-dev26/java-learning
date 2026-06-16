03f06d9c920271bd2446c12d498a8ccf21359f06
kishan-dev26 <kishancode26@gmail.com>
2026-06-16 22:45:35 +0530

    Update Variables.java

--- Day-02/Variables.java
+++ Day-02/Variables.java
@@ -1,1 +1,19 @@
+public class Variables{
+  public static void main(String[] args)
+  {
+    int age=20;
+    double marks=92.5;
+    char grade='A';
+    boolean isStudent=true;
+    String name="Kishan";
+
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(marks);
+    System.out.println(grade);
+    System.out.println(isStudent);
+  }
+}
+
+
 
