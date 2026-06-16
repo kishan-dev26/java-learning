5be779ff1764167903dae14380a888df03c83587
kishan-dev26 <kishancode26@gmail.com>
2026-06-16 23:06:40 +0530

    Update StudentDetails.java

--- Day-02/StudentDetails.java
+++ Day-02/StudentDetails.java
@@ -1,1 +1,13 @@
+public class StudentDetails{
+  public static void main(String[]args)
+  {
+    String name="Kishan";
+    int age=20;
+    String college="Jain polytechnic Davanagere";
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(college);
+  }
+}
 
+    
